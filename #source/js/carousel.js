a27a4e0b449dbe78a4b268d9af4aa8a42a7e918c
Sergey-Chernushevich <sergeychernushevich@gmail.com>
2024-11-02 12:56:29 +0300

    feat: add favicon

--- #source/js/carousel.js
+++ #source/js/carousel.js
@@ -1,27 +1,37 @@
-const reviewsCatousel = new Swiper(".reviewsCatousel", {
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 32,
-  autoHeight: true,
+window.addEventListener("DOMContentLoaded", () => {
+  const reviewsCarousel = new Swiper(".reviewsCatousel", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 32,
+    autoHeight: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    observer: true,
+    observeParents: true,
 
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-
-  navigation: {
-    nextEl: ".handler-next",
-    prevEl: ".handler-prev",
-  },
-
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-      autoHeight: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     },
-    1500: {
-      slidesPerView: 3,
-      autoHeight: false,
+
+    navigation: {
+      nextEl: ".handler-next",
+      prevEl: ".handler-prev",
     },
-  },
+
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        autoHeight: false,
+      },
+      1500: {
+        slidesPerView: 3,
+        autoHeight: false,
+      },
+    },
+  });
+
+  window.addEventListener("resize", () => {
+    reviewsCarousel.update();
+  });
 });
